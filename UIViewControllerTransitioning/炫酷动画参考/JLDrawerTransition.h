1bea72b05c747bb368358c25644f892fd9819ba6
DuWenLiang <739285514@qq.com>
2017-12-20 15:43:46 +0800

    暂存一些炫酷算法动画做参考

---
+++ UIViewControllerTransitioning/炫酷动画参考/JLDrawerTransition.h
@@ -0,0 +1,13 @@
+//
+//  JLDrawerTransition.h
+//  JLTransition
+//
+//  Created by joshuali on 16/6/29.
+//  Copyright © 2016年 joshuali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+@interface JLDrawerTransition : NSObject<UIViewControllerAnimatedTransitioning>
+@end
